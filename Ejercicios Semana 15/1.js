9487f6d3be16172848fed34a1ee964b5b0c8d594
Adrian <ajmunoz@sudamericano.edu.ec>
2026-02-10 19:11:51 -0500

    REALIZAMOS LA PRIMERA TAREA

---
+++ Ejercicios Semana 15/1.js
@@ -0,0 +1,12 @@
+console.log();
+{
+    const nums = [7, 4, 0, 9, 7, 6, 3, 8];
+    let contador = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        contador++;
+    }
+
+    console.log('Array:', nums);
+    console.log('Tamaño del array:', contador);
+}
